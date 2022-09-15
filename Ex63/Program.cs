6cd854166794a91138e214853c5d7ae452611d5c
MAX <maksimdemidovru@mail.ru>
2022-09-16 00:10:11 +0300

    add 63,69

---
+++ Ex63/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Введите число: ");
+bool line = int.TryParse(Console.ReadLine(), out int a);
+// Console.WriteLine("Введите количество строк в массиве: ");
+// bool column = int.TryParse(Console.ReadLine(), out int n);
+
+if (!line || a < 1 )
+{
+    Console.WriteLine("Не верные введённые данные, попробуйте поменять значение.");
+}
+
+int[] newArray = new int[a]; 
+newArray = Recursive(a,0,1,newArray);
+PrintArray(newArray);
+int[] Recursive(int a , int i,int m ,int[]array )
+{
+    if (i>a-1)
+    {
+        return array;
+    }
+     array[i] = m;
+    Recursive(a,i+1,m+1, array);
+    return  array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($" {array[i]} ");
+    }
+}
---
+++ Ex69/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите число: ");
+bool num1 = int.TryParse(Console.ReadLine(), out int a);
+Console.WriteLine("Введите число: ");
+bool num2 = int.TryParse(Console.ReadLine(), out int b);
+
+if (!num1 || !num2  )
+{
+    Console.WriteLine("Не верные введённые данные, попробуйте поменять значение.");
+}
+PrintResult(GetSum(a,b));
+int GetSum(int a, int b,int i = 1)
+{
+    if(i<=b)
+    {
+    
+    i++;
+     return  a* GetSum(a,b,i);
+     
+    }
+   return 1;
+}
+
+void PrintResult(int i)
+{
+    Console.WriteLine(i);
+}
