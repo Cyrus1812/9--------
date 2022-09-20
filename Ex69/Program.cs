e6a46af65b985e08dd77e9083cece5fc89697840
MAX <maksimdemidovru@mail.ru>
2022-09-20 10:49:03 +0300

    69

--- Ex69/Program.cs
+++ Ex69/Program.cs
@@ -1,12 +1,14 @@
-﻿Console.WriteLine("Введите число: ");
-bool num1 = int.TryParse(Console.ReadLine(), out int a);
-Console.WriteLine("Введите число: ");
-bool num2 = int.TryParse(Console.ReadLine(), out int b);
+﻿// Console.WriteLine("Введите число: ");
+// bool num1 = int.TryParse(Console.ReadLine(), out int a);
+// Console.WriteLine("Введите число: ");
+// bool num2 = int.TryParse(Console.ReadLine(), out int b);
 
-if (!num1 || !num2  )
-{
-    Console.WriteLine("Не верные введённые данные, попробуйте поменять значение.");
-}
+// if (!num1 || !num2  )
+// {
+//     Console.WriteLine("Не верные введённые данные, попробуйте поменять значение.");
+// }
+int a = 4;
+int b = 10;
 PrintResult(GetSum(a,b));
 int GetSum(int a, int b,int i = 1)
 {
@@ -14,10 +16,10 @@ int GetSum(int a, int b,int i = 1)
     {
     
     i++;
-     return  a* GetSum(a,b,i);
+     return  a+ GetSum(a,b,i);
      
     }
-   return 1;
+   return a;
 }
 
 void PrintResult(int i)
